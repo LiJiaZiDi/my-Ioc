251a201a97d30bbfff759e6decb912997c497f39
lph <liph@fosun.com>
2018-04-24 16:47:54 +0800

    首次提交

---
+++ src/main/java/com/lph/myIoc/App.java
@@ -0,0 +1,13 @@
+package com.lph.myIoc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
